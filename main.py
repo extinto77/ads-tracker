784b6ceb32c8c51d8b242489b5471029335cee3c
pedrommf7 <pedro2mferreira@gmail.com>
2023-04-10 01:21:11 +0100

    nr count

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,16 +19,20 @@ caps['goog:loggingPrefs'] = {'performance': 'ALL'}
 driver = webdriver.Chrome(desired_capabilities=caps)
 driver.get('http://jn.pt')
 
-browser_log = driver.get_log('performance') 
-events = [process_browser_log_entry(entry) for entry in browser_log]
-events = [event for event in events if 'Network.response' in event['method']]
+sleep(117)
 
-#inisert the output in a file
-with open('output.txt', 'w') as f:
-    #for item in events:
-    output = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[0]['params']['requestId']}) 
-    for k,item in output.items():
-        f.write(k+':'+str(item)+'\n')
+# browser_log = driver.get_log('performance') 
+# events = [process_browser_log_entry(entry) for entry in browser_log]
+# events = [event for event in events if 'Network.response' in event['method']]
+
+# #inisert the output in a file
+# for i in range(len(events)):
+#     print(events[i]['params']['requestId'])
+# with open('output'+''+'.txt', 'w') as f:
+#     #for item in events:
+#     output = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[16]['params']['requestId']}) 
+#     for k,item in output.items():
+#         f.write(k+':'+str(item)+'\n')
 
 #for i in range(len(events)):
 #    print(driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[i]['params']['requestId']}))
@@ -43,3 +48,21 @@ with open('output.txt', 'w') as f:
 
 
 # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.button__acceptAll > span.baseText"))).click()
+
+
+logs_raw = driver.get_log("performance")
+logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+
+def log_filter(log_):
+    return (
+        # is an actual response
+        log_["method"] == "Network.responseReceived"
+        # and json
+        and "json" in log_["params"]["response"]["mimeType"]
+    )
+
+for log in filter(log_filter, logs):
+    request_id = log["params"]["requestId"]
+    resp_url = log["params"]["response"]["url"]
+    print(f"Caught {resp_url}")
+    #print(driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id}))
