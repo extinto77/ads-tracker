e3dc6f16d6a1234fccba357740b9e22946aa04ec
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2023-04-27 18:10:42 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,68 +1,119 @@
-from time import sleep
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import requests
+import sys
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import json
+from selenium.webdriver.support.ui import WebDriverWait
 
+dialog_selector = '#dialogOverlay-0 > groupbox:nth-child(1) > browser:nth-child(2)'
 
-def process_browser_log_entry(entry):
-    response = json.loads(entry['message'])['message']
-    return response
+accept_dialog_script = (
+    f"const browser = document.querySelector('{dialog_selector}');" +
+    "browser.contentDocument.documentElement.querySelector('#clearButton').click();"
+)
 
+def get_clear_site_data_button(driver):
+    return driver.find_element_by_css_selector('#clearSiteDataButton')
 
+def get_clear_site_data_dialog(driver):
+    return driver.find_element_by_css_selector(dialog_selector)
 
-caps = DesiredCapabilities.CHROME
-caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-driver = webdriver.Chrome(desired_capabilities=caps)
-driver.get('http://jn.pt')
+def get_clear_site_data_confirmation_button(driver):
+    return driver.find_element_by_css_selector('#clearButton')
 
-sleep(117)
+def clear_firefox_cache(driver, timeout=10):
+    driver.get('about:preferences#privacy')
+    wait = WebDriverWait(driver, timeout)
 
-# browser_log = driver.get_log('performance') 
-# events = [process_browser_log_entry(entry) for entry in browser_log]
-# events = [event for event in events if 'Network.response' in event['method']]
+    # Click the "Clear Data..." button under "Cookies and Site Data".
+    wait.until(get_clear_site_data_button)
+    get_clear_site_data_button(driver).click()
 
-# #inisert the output in a file
-# for i in range(len(events)):
-#     print(events[i]['params']['requestId'])
-# with open('output'+''+'.txt', 'w') as f:
-#     #for item in events:
-#     output = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[16]['params']['requestId']}) 
-#     for k,item in output.items():
-#         f.write(k+':'+str(item)+'\n')
+    # Accept the "Clear Data" dialog by clicking on the "Clear" button.
+    wait.until(get_clear_site_data_dialog)
+    driver.execute_script(accept_dialog_script)
 
-#for i in range(len(events)):
-#    print(driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[i]['params']['requestId']}))
-#    # driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events["params"]["requestId"]})
+    # Accept the confirmation alert.
+    wait.until(EC.alert_is_present())
+    alert = Alert(driver)
+    alert.accept()
 
+if(len(sys.argv) < 2):
+    print("Usage: python main.py <page_url>")
+    exit()
 
+#Get Program Args
+page_url = sys.argv[1]
 
-# browser = webdriver.Firefox()
-# info = browser.get("http://jn.pt")
-# info = browser.find_elements(By.CLASS_NAME,"fc-button fc-cta-consent fc-primary-button")[0]
-# info.click()
-# print(info)
+print("Loading Ads Servers List...")
+ADS_LIST_URL = "https://pgl.yoyo.org/as/serverlist.php?hostformat=adblockplus;showintro=0"
 
+#Get Ads Servers
+ads_list_page = requests.get(ADS_LIST_URL)
+ads_list = ads_list_page.text.splitlines()
+#Remove lines that are not ads servers
+ads_list = [x for x in ads_list if x.startswith('||')]
+#Remove the || from the beginning of the line
+ads_list = [x.replace('||', '') for x in ads_list]
+#Remove the ^ from the end of the line
+ads_list = [x.replace('^', '') for x in ads_list]
+print("Ads Servers List Loaded!")
 
-# WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.button__acceptAll > span.baseText"))).click()
+# Create a new instance of the Firefox driver
+options = ChromeOptions()
+options.add_argument("--headless")
+options.add_extension("uBlock_extension.crx")
+driver = webdriver.Chrome(
+    options = options
+)
+driver.delete_all_cookies()
 
+# Go to the url page
+driver.get(page_url)
+print("=====================================")
+print("Loading page: ",page_url)
 
-logs_raw = driver.get_log("performance")
-logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+total_downloaded_bytes = 0
+total_ads_bytes = 0
+total_requests_number = 0
+total_ads_number = 0
 
-def log_filter(log_):
-    return (
-        # is an actual response
-        log_["method"] == "Network.responseReceived"
-        # and json
-        and "json" in log_["params"]["response"]["mimeType"]
-    )
+def is_ad_request(request):
+    request_url = request.url
 
-for log in filter(log_filter, logs):
-    request_id = log["params"]["requestId"]
-    resp_url = log["params"]["response"]["url"]
-    print(f"Caught {resp_url}")
-    #print(driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id}))
+    # Check if the request URL contains any of the ad server names
+    for ad_server in ads_list:
+        if ad_server in request_url:
+            #print(request.date," | ",request.response.status_code," | ",request.url,)
+            return True
+        
+    return False
+
+# Access requests via the `requests` attribute
+for request in driver.requests:
+    if request.response and request.response.status_code == 200:
+        response_size = 0
+        try:
+            response_size = int(request.response.headers['content-length'])
+        except:
+            pass
+
+        total_downloaded_bytes += int(response_size)
+        total_requests_number += 1
+        if is_ad_request(request):
+            #print(request.date," | ",request.response.status_code," | ",request.url,)
+            total_ads_bytes += int(response_size)
+            total_ads_number += 1
+
+total_downloaded_mb = total_downloaded_bytes / 1024 / 1024
+total_ads_mb = total_ads_bytes / 1024 / 1024
+percentage_of_ads = total_ads_bytes / total_downloaded_bytes * 100
+
+print('Total downloaded: %.2f MB' % total_downloaded_mb)
+print('Total ads: %.2f MB (%.2f%%)' % (total_ads_mb, percentage_of_ads))
+print('Total requests: %d' % total_requests_number)
+print('Total ads requests: %d' % total_ads_number)
+
+driver.close()
