2ffeea8bd95e98ac6934e04c7902656b065be0bc
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2023-05-08 16:34:22 +0100

    Refactoring, testes multiplos loads

--- main.py
+++ main.py
@@ -29,23 +29,29 @@ print("Ads Servers List Loaded!")
 def start_driver():
     # Create a new instance of the Chrome driver
     options = ChromeOptions()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
+
     options.add_extension("uBlock_extension.crx")
+    #options.add_extension("adGuard.crx")
+    #options.add_extension("adBlock.crx")
+
     driver = webdriver.Chrome(
         options = options
     )
     return driver
 
-# Go to the url page
-driver = start_driver()
-driver.get(page_url)
-print("=====================================")
-print("Loading page: ",page_url)
+def load_page(page_url, driver):
+    print("=====================================")
+    print("Loading page: ",page_url)
+    
+    driver.switch_to.new_window('tab')
+    driver.delete_all_cookies()
+    driver.get(page_url)
+    
+    driver_requests = driver.requests.copy()
+    del driver.requests
 
-total_downloaded_bytes = 0
-total_ads_bytes = 0
-total_requests_number = 0
-total_ads_number = 0
+    return driver_requests
 
 def is_ad_request(request):
     request_url = request.url
@@ -58,33 +64,54 @@ def is_ad_request(request):
         
     return False
 
-# Access requests via the `requests` attribute
-for request in driver.requests:
-    print(str(request.response.status_code) + " | " + request.url)
-    if(is_ad_request(request)):
-        print("AD DETECTED!")
-    if request.response and request.response.status_code == 200:
-        response_size = 0
-        try:
-            response_size = int(request.response.headers['content-length'])
-        except:
-            pass
+def analyse_page_load(driver_requests):
+    total_downloaded_bytes = 0
+    total_ads_bytes = 0
+    total_requests_number = 0
+    total_ads_number = 0
+    
+    # Access requests via the `requests` attribute
+    for request in driver_requests:
+        #print(str(request.response.status_code) + " | " + request.url)
+        #if(is_ad_request(request)):
+            #print("AD DETECTED!")
+        if request.response and request.response.status_code == 200:
+            response_size = 0
+            try:
+                response_size = int(request.response.headers['content-length'])
+            except:
+                pass
 
-        total_downloaded_bytes += int(response_size)
-        total_requests_number += 1
-        if is_ad_request(request):
-            #print(request.date," | ",request.response.status_code," | ",request.url,)
-            total_ads_bytes += int(response_size)
-            total_ads_number += 1
+            total_downloaded_bytes += int(response_size)
+            total_requests_number += 1
+            if is_ad_request(request):
+                #print(request.date," | ",request.response.status_code," | ",request.url,)
+                total_ads_bytes += int(response_size)
+                total_ads_number += 1
 
-total_downloaded_mb = total_downloaded_bytes / 1024 / 1024
-total_ads_mb = total_ads_bytes / 1024 / 1024
-percentage_of_ads = total_ads_bytes / total_downloaded_bytes * 100
+    total_downloaded_mb = total_downloaded_bytes / 1024 / 1024
+    total_ads_mb = total_ads_bytes / 1024 / 1024
+    percentage_of_ads = total_ads_bytes / total_downloaded_bytes * 100
 
-print('Total downloaded: %.2f MB' % total_downloaded_mb)
-print('Total ads: %.2f MB (%.2f%%)' % (total_ads_mb, percentage_of_ads))
-print('Total requests: %d' % total_requests_number)
-print('Total ads requests: %d' % total_ads_number)
+    print('Total downloaded: %.2f MB' % total_downloaded_mb)
+    print('Total ads: %.2f MB (%.2f%%)' % (total_ads_mb, percentage_of_ads))
+    print('Total requests: %d' % total_requests_number)
+    print('Total ads requests: %d' % total_ads_number)
+    print("=====================================")
 
-driver.close()
+
+driver = start_driver()
+
+fst_load = load_page(page_url, driver)
+analyse_page_load(fst_load)
+
+snd_load = load_page(page_url, driver)
+analyse_page_load(snd_load)
+
+third_load = load_page(page_url, driver)
+analyse_page_load(third_load)
+
+fourth_load = load_page(page_url, driver)
+analyse_page_load(fourth_load)
+
 driver.quit()
